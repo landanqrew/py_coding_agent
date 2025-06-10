df37f4c932e1e2696ef7bda23e20e296cdf13865
Landan Quartemont <landan@grease.solutions>
2025-06-10 17:57:04 -0500

    updated to include prompt from command line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv(dotenv_path="secrets/secrets.env")
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,10 +14,22 @@ def get_llm_response(prompt: str) -> str:
     response = client.models.generate_content(model=gemini_model, contents=prompt)
     return {"response": response.text , "Prompt tokens": response.usage_metadata.prompt_token_count, "Response tokens": response.usage_metadata.candidates_token_count}
 
+def main():
+    command_line_args = sys.argv
+    if len(command_line_args) <= 1:
+        print("no prompt included")
+        sys.exit(1)
+    print(command_line_args)
+    
+    '''response: str = get_llm_response(prompt)
+    for key, value in response.items():
+        print(f"{key}: {value}")'''
 
 if __name__ == "__main__":
     # prompt: str = "give me a cool prompt to ask an AI"
     prompt: str = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response: str = get_llm_response(prompt)
+    main()
+    
+    '''response: str = get_llm_response(prompt)
     for key, value in response.items():
-        print(f"{key}: {value}")
+        print(f"{key}: {value}")'''
